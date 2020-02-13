e78e700383b029c00f9d3866e6e0b099b5a46dd2
Tiffany Obi <tiffanyobi123@gmail.com>
2020-02-13 11:09:47 -0500

    completed Tests on api and struct. completed Model. completed APi client

--- NYTBGroupProject/Models/NYTBookStruct.swift
+++ NYTBGroupProject/Models/NYTBookStruct.swift
@@ -7,3 +7,79 @@
 //
 
 import Foundation
+
+struct SearchResult: Codable {
+               
+    let results: [BookTopics]
+               
+}
+struct BookTopics: Codable {
+    let listname: String
+    let listNameEncoded: String
+    let updated: String
+}
+extension BookTopics {
+    
+    enum CodingKeys: String,CodingKey {
+        case listname = "list_name"
+        case listNameEncoded = "list_name_encoded"
+        case updated
+    }
+    
+}
+
+struct FilteredBooks: Codable {
+    let numResults: Int
+    let results: Details
+  
+
+
+    enum CodingKeys: String,CodingKey {
+    case numResults = "num_results"
+    case results
+   
+}
+}
+
+struct Details: Codable {
+        let displayname: String
+        let bestsellersDate: String
+        let books : [Books]
+    }
+extension Details {
+    enum CodingKeys: String, CodingKey {
+        case displayname = "display_name"
+        case bestsellersDate = "bestsellers_date"
+        case books
+    }
+}
+
+struct Books: Codable {
+    let rank: Int
+    let title: String
+    let publisher: String
+    let author: String
+    let contributor: String
+    let bookImage: String
+    let buyLinks: [Links]
+    
+    
+}
+
+extension Books {
+    enum CodingKeys: String, CodingKey {
+        case rank
+        case contributor
+        case title
+        case publisher
+        case author
+        case bookImage = "book_image"
+        case buyLinks = "buy_links"
+    }
+    
+}
+
+struct Links: Codable {
+    let name: String
+    let url: String
+}
--- NYTBGroupProject/Network Services/NYTAPIClient.swift
+++ NYTBGroupProject/Network Services/NYTAPIClient.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+import NetworkHelper
+
+struct NYTApiClient {
+    static func getTopics(completion: @escaping (Result<[BookTopics], AppError>)->()) {
+        let endpointURLString = "https://api.nytimes.com/svc/books/v3//lists/names.json?api-key=\(ApiKey.apiKey)"
+        guard let url = URL(string: endpointURLString) else {
+            completion(.failure(.badURL(endpointURLString)))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result{
+            case .failure(let error):
+                completion(.failure(.networkClientError(error)))
+            case .success(let data):
+                do{
+                    let searchResearch = try JSONDecoder().decode(SearchResult.self, from: data)
+                    let topics = searchResearch.results
+                    completion(.success(topics))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+    
+    static func getBooks(from topic: BookTopics, completion: @escaping (Result<[Books],AppError>)->()) {
+        
+        
+        let endpointURL = "https://api.nytimes.com/svc/books/v3/lists/current/\(topic.listNameEncoded).json?api-key=\(ApiKey.apiKey)"
+        
+        guard let url = URL(string: endpointURL) else {
+            completion(.failure(.badURL(endpointURL)))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+                
+            case .success(let data):
+                do {
+                   let books = try JSONDecoder().decode(FilteredBooks.self, from: data)
+                    let filteredBooks = books.results.books
+                    completion(.success(filteredBooks))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
